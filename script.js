4d7dcf016b9033ec031255e3f49f9a645bf33430
Leonardo <stvrshxpping@gmail.com>
2024-06-20 14:15:38 -0300

    Exercicio jQuery

--- script.js
+++ script.js
@@ -1,25 +1,17 @@
-document.getElementById('button').addEventListener('click', function(event){
-    event.preventDefault();
+$(document).ready(function(){
+    $('#btnCadastrar').on('click', function(e){
+        e.preventDefault();
+        var inputValue = $('#inputName').val();
+        if(inputValue.length > 0){
+            addTarefa(inputValue);
+        }
+    });
 
-    const valorA = parseFloat(document.querySelector('#inputA').value);
-    const valorB = parseFloat(document.querySelector('#inputB').value);
-    const perfeito = document.getElementById('perfeito');
-    const errado = document.getElementById('errado');
-    const pane = document.getElementById('pane');
+    function addTarefa(text){
+        $('#taskList').append('<li>' + text + '</li>')
+    }
 
-    if(valorB > valorA){
-        perfeito.style.display = 'block';
-        errado.style.display = 'none';
-        pane.style.display = 'none';
-    }
-    else if(valorA > valorB){
-        errado.style.display = 'block';
-        perfeito.style.display = 'none';
-        pane.style.display = 'none';
-    }
-    else if(valorA == valorB){
-        pane.style.display = 'block';
-        perfeito.style.display = 'none';
-        errado.style.display = 'none';
-    }
-});
+    $(document).on('click', 'li', function(){
+        $(this).toggleClass('completed');
+    })
+})
